148e9fd233cddb1b1d3c941d61c287855b2b4240
Antonio Lucena <lucenacalderon90@outlook.es>
2026-05-27 23:17:03 +0200

    fix: apply forge fmt formatting

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -5,21 +5,10 @@ pragma solidity ^0.8.34;
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract StakingApp {
+    // Variables
+    address public StakingToken;
 
-    // Variables 
-      address public StakingToken;
-    constructor(address stakingToken_, address admin_){
+    constructor(address stakingToken_, address admin_) {
         stakingToken = stakingToken_;
-
     }
-
-
-
-
-
-
-
-
-
-
 }
