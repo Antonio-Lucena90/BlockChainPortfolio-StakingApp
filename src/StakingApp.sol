4c90e68a44206cc23b7c5918f9087bea0c3501e9
Antonio Lucena <lucenacalderon90@outlook.es>
2026-05-27 21:00:47 +0200

    first commit

---
+++ src/StakingApp.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.34;
+
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract StakingApp {
+
+    // Variables 
+      address public StakingToken;
+    constructor(address stakingToken_, address admin_){
+        stakingToken = stakingToken_;
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
