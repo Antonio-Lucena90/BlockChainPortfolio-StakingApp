4d42c4f2bd96c606abb87c791f595a883b98f279
Antonio Lucena <lucenacalderon90@outlook.es>
2026-05-28 22:22:23 +0200

    stakingToken

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.34;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract StakingApp {
+contract StakingApp is Ownable {
     // Variables
     address public StakingToken;
 
-    constructor(address stakingToken_, address admin_) {
+    constructor(address stakingToken_, address owner_) Ownable(owner_){
         StakingToken = stakingToken_;
     }
 }
---
+++ test/StakingTokenTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
+
+import "forge-std/Test.sol";
+import "../src/StakingToken.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract StakingTokenTest is Test {
+
+  StakingToken stakingToken;
+  string name_ = "StakingToken";
+  string symbol_ = "STK";
+  address ramdonUser = vm.addr(1); 
+  
+  function setUp() public {
+    stakingToken = new StakingToken (name_, symbol_);
+  }
+
+  function testStakingTokenMintsCorrectly() public{
+    vm.startPrank(ramdonUser);
+    uint256 amount_ = 1 ether;
+
+    //TokenBalance Previous
+    uint256 balanceBefore_ = IERC20(address(stakingToken)).balanceOf(ramdonUser);
+
+    stakingToken.mint(amount_);
+    
+    //TokenBalance after
+    uint256 balanceAfter_ = IERC20(address(stakingToken)).balanceOf(ramdonUser);
+
+    assert(balanceAfter_ - balanceBefore_ == amount_);
+    
+    vm.stopPrank();
+  }
+}
